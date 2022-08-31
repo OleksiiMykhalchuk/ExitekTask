69fb8bc56710d4f912ee5614ec385d89731771e4
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-08-31 14:57:30 +0200

    create outlets and action in ViewController

--- ExitekTask/ViewController.swift
+++ ExitekTask/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     var viewModel: ViewModel = ViewModel()
 
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var addButtonOutlet: UIButton!
+    @IBOutlet weak var tableView: UITableView!
 
-
+    @IBAction func addButtonAction(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
