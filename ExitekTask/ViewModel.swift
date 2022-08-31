660795eba6b0cfe2c9edf2c77697ce330faff208
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-08-31 14:42:55 +0200

    add ViewModel

---
+++ ExitekTask/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModel.swift
+//  ExitekTask
+//
+//  Created by Oleksii Mykhalchuk on 8/31/22.
+//
+
+import Foundation
+
+extension ViewController {
+    class ViewModel {
+        
+    }
+}
