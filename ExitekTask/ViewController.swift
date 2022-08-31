a77cbccbfffd2b78bac16622ecd08a8a84d29da3
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-08-31 15:10:02 +0200

    tableViewDataSource

--- ExitekTask/ViewController.swift
+++ ExitekTask/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     var viewModel: ViewModel = ViewModel()
     private var cornerRadius: CGFloat = 6
-
+// MARK: - Outlets
     @IBOutlet weak var yearTextField: UITextField!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var addButtonOutlet: UIButton! {
@@ -19,11 +19,22 @@ class ViewController: UIViewController {
         }
     }
     @IBOutlet weak var tableView: UITableView!
-
+// MARK: - Action
     @IBAction func addButtonAction(_ sender: Any) {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+    }
+}
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "default")
+        cell.textLabel?.text = "Movie Title Year"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
 }
